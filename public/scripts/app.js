bb5a52693c6e16aa487cae472532977d9287c641
awmiri <talasaz1382@gmail.com>
2025-02-11 00:34:59 +0330

    add local arry or data base for web

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,3 +1,13 @@
+let allCountryArry = [
+    { id: 1, img: "../imge/imge/turky/image 37.png", toreName: "تور آنتالیا", price: 45000000, score: 4.1, day: 3, night: 4 },
+    { id: 2, img: "../imge/imge/malezi/image 39.png", toreName: 'تور دبی', price: 4.9, score: 65000000, day: 4, night: 5 },
+    { id: 3, img: "../imge/imge/dubai/Frame 31.png", toreName: 'تور مالزی', price: 4.1, score: 70000000, day: 6, night: 5 }
+]
+
+let headerImgArry = [
+    { id: 1, nameCountry: "تایلند", capital: "پاتایا", imgSrc: "../imge/imge/image 2.png" },
+    { id: 2, nameCountry: "دبی", capital: "مارات متحده عربی", imgSrc: "../imge/imge/dubai/RBP06201.jpg" },
+]
 let $ = document
 
 let selectBar = $.querySelector('#fa-bar')
