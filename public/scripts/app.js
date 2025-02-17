8f72fec3f2d947c55a5ef024ef362ddeae9dcf45
awmiri <talasaz1382@gmail.com>
2025-02-17 20:35:35 +0330

    add new contry to arry and header

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,8 @@
 let allCountryArry = [
-    { id: 1, img: "../imge/imge/turky/image 37.png", toreName: "تور آنتالیا", price: 45000000, score: 4.1, day: 3, night: 4 },
-    { id: 2, img: "../imge/imge/malezi/image 39.png", toreName: 'تور دبی', price: 4.9, score: 65000000, day: 4, night: 5 },
-    { id: 3, img: "../imge/imge/dubai/Frame 31.png", toreName: 'تور مالزی', price: 4.1, score: 70000000, day: 6, night: 5 }
+    { id: 1, img: "../imge/imge/turky/image 37.png", toreName: "تور آنتالیا", price: 45000000, score: 4.1, day: 3, night: 4, discount: true },
+    { id: 2, img: "../imge/imge/malezi/image 39.png", toreName: 'تور دبی', price: 4.9, score: 65000000, day: 4, night: 5, discount: false },
+    { id: 3, img: "../imge/imge/dubai/Frame 31.png", toreName: 'تور مالزی', price: 4.1, score: 70000000, day: 6, night: 5, discount: true },
+    { id: 3, img: "../imge/imge/dubai/Frame 31.png", toreName: 'اصفهان', price: 4.4, score: 15000000, day: 6, night: 5, discount: true },
 ]
 
 let headerImgArry = [
@@ -9,6 +10,7 @@ let headerImgArry = [
     { id: 2, nameCountry: "امارات متحده عربی", capital: "دبی", imgSrc: "../imge/imge/dubai/RBP06201.jpg" },
     { id: 3, nameCountry: "ایتالیا", capital: "میلان", imgSrc: "../imge/imge/italy/view-Milan-Italy.webp" },
     { id: 4, capital: "مالزی", nameCountry: "کوالانپور", imgSrc: "../imge/imge/malezi/Malaysia-Kuala-Lumpur.jpg" },
+    { id: 5, capital: "ایران", nameCountry: "اصفهان", imgSrc: "../imge/imge/esfahan/esfahan-header.jpg" },
 ]
 let $ = document
 
